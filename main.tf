cb2c2b1159b6e6e567123788217e6d4a79b2a9bf
Eun Song <songew81@gmail.com>
2020-03-20 16:54:07 +0900

    wip

--- main.tf
+++ main.tf
@@ -2,39 +2,49 @@
 ## S3 Bucket for Lambda
 ###########################################################
 resource "aws_s3_bucket" "lambda" {
-  count  = "${var.create_s3_bucket ? 1 : 0}"
-  bucket = "${var.s3_bucket_name}"
+  count  = var.create_s3_bucket ? 1 : 0
+  bucket = var.s3_bucket_name
   acl    = "private"
-  tags   = "${var.aws_s3_bucket_tags}"
+  tags   = var.aws_s3_bucket_tags
 
-  versioning { enabled = "${var.s3_bucket_versioning_enabled}" } 
+  versioning {
+    enabled = var.s3_bucket_versioning_enabled
+  }
 }
 
 ###########################################################
 ## Lambda
 ###########################################################
 resource "aws_lambda_function" "lambda" {
-  function_name = "${var.function_name}"
-  s3_bucket     = "${var.s3_bucket_name}"
+  function_name = var.function_name
+  s3_bucket     = var.s3_bucket_name
   s3_key        = "${var.build_version}/${var.build_file_name}"
-  handler       = "${var.handler_name}"
-  runtime       = "${var.runtime}"
-  memory_size   = "${var.memory_size}"
-  timeout       = "${var.timeout}"
-  role          = "${aws_iam_role.lambda.arn}"
+  handler       = var.handler_name
+  runtime       = var.runtime
+  memory_size   = var.memory_size
+  timeout       = var.timeout
+  role          = aws_iam_role.lambda.arn
 
   vpc_config {
-    security_group_ids = ["${var.vpc_config["security_group_ids"]}"]
-    subnet_ids         = ["${var.vpc_config["subnet_ids"]}"]     
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibility in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    security_group_ids = [var.vpc_config["security_group_ids"]]
+    subnet_ids = [var.vpc_config["subnet_ids"]]
   }
 
   environment {
-    variables = "${var.environment}"
+    variables = var.environment
   }
 }
 
 resource "aws_iam_role" "lambda" {
-  name = "${var.project_name}"
+  name = var.project_name
 
   assume_role_policy = <<EOF
 {
@@ -51,11 +61,12 @@ resource "aws_iam_role" "lambda" {
   ]
 }
 EOF
+
 }
 
 resource "aws_iam_role_policy" "lambda_logging" {
   name = "${var.project_name}-lambda-logging-policy"
-  role = "${aws_iam_role.lambda.id}"
+  role = aws_iam_role.lambda.id
 
   policy = <<EOF
 {
@@ -72,4 +83,6 @@ resource "aws_iam_role_policy" "lambda_logging" {
   ]
 }
 EOF
+
 }
+
--- vars.tf
+++ vars.tf
@@ -1,29 +1,56 @@
-variable "build_version"                {                       } 
-variable "build_file_name"              {                       } 
-variable "project_name"                 {                       } 
-variable "s3_bucket_name"               {                       } 
-variable "function_name"                {                       } 
-variable "handler_name"                 {                       } 
-variable "s3_bucket_versioning_enabled" { default = true        }
-variable "memory_size"                  { default = 384         }
-variable "timeout"                      { default = 200         }
-variable "runtime"                      { default = "python3.6" }
-variable "create_s3_bucket"             { default = true        }
-
-variable "aws_s3_bucket_tags" { 
-  type    = "map"       
-  default = { }
+variable "build_version" {
 }
 
-variable "environment" { 
-  type    = "map"
-  default = { }
+variable "build_file_name" {
 }
 
-variable "vpc_config" { 
-  type    = "map"
-  default = { 
+variable "project_name" {
+}
+
+variable "s3_bucket_name" {
+}
+
+variable "function_name" {
+}
+
+variable "handler_name" {
+}
+
+variable "s3_bucket_versioning_enabled" {
+  default = true
+}
+
+variable "memory_size" {
+  default = 384
+}
+
+variable "timeout" {
+  default = 200
+}
+
+variable "runtime" {
+  default = "python3.6"
+}
+
+variable "create_s3_bucket" {
+  default = true
+}
+
+variable "aws_s3_bucket_tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "environment" {
+  type    = map(string)
+  default = {}
+}
+
+variable "vpc_config" {
+  type = map(string)
+  default = {
     security_group_ids = []
-    subnet_ids         = []     
+    subnet_ids         = []
   }
-} 
+}
+
