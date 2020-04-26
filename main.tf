47dcc8bb7d2cf6d0bc7751c5e0fd75938db6764a
Eun Woo Song <songew@gmail.com>
2020-04-26 19:57:53 +0900

    KMS for lambda
    Added resources for KMS key management.
    It creaes KMS for lambda to use.
    More specifically, KMS will be used to decrypt enviroment values
    so secret keys can be securely used in the lambda code.

--- main.tf
+++ main.tf
@@ -13,6 +13,60 @@ resource "aws_s3_bucket" "lambda" {
 }
 
 ###########################################################
+## KMS For Lambda
+###########################################################
+data "aws_caller_identity" "current" { }
+data "aws_iam_policy_document" "kms_policy" {
+  ## IAM policy for KMS needs to have two statements:
+  ## One for the current account (the actual user)
+  ## and the other for the lambda iam role.
+  ## For the current account, everything should be allowed.
+  ## And the lambda iam role, only needed actions should be allowed, 
+  ## which is to decrypt this case.
+  statement {
+    sid        = "1"
+    effect     = "Allow"
+    actions    = ["kms:*"]
+    resources  = ["*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+
+  statement {
+    effect     = "Allow"
+    actions    = ["kms:Decrypt"]
+    resources  = ["*"]
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.lambda.arn]
+    }
+  }
+}
+
+resource "aws_kms_key" "lambda" {
+  description         = "KSM Key For ${var.project_name} on AWS Lambda"
+  is_enabled          = true
+  enable_key_rotation = true
+  policy              = data.aws_iam_policy_document.kms_policy.json
+}
+
+##
+## It would be nice to be able to use aliase for kms.
+## But apparently, as of 2020 April, kms alise does not
+## work for lambda as it throws the following error:
+##
+## InvalidParameterValueException: Lambda was unable to configure access to your environment variables 
+## because the KMS key is invalid for CreateGrant. Please check your KMS key settings. 
+## KMS Exception: InvalidArnException KMS Message: Key Aliases are not supported for this operation.
+##
+#resource "aws_kms_alias" "lambda" {
+#  name          = "alias/${var.project_name}"
+#  target_key_id = aws_kms_key.lambda.key_id
+#}
+
+###########################################################
 ## Lambda
 ###########################################################
 resource "aws_lambda_function" "lambda" {
@@ -34,6 +88,8 @@ resource "aws_lambda_function" "lambda" {
   environment {
     variables = var.environment
   }
+
+  kms_key_arn = aws_kms_key.lambda.arn
 }
 
 resource "aws_iam_role" "lambda" {
@@ -56,6 +112,37 @@ resource "aws_iam_role" "lambda" {
 EOF
 
 }
+
+#resource "aws_iam_role_policy" "lambda_kms_access" {
+#  name = "${var.project_name}-kms-access-policy"
+#  role = aws_iam_role.lambda.id
+
+#  policy = <<EOF
+#{
+#  "Version": "2012-10-17",
+#  "Statement": [
+#    {
+#      "Effect": "Allow",
+#      "Action": [
+#        "kms:Create*",
+#        "kms:Describe*",
+#        "kms:Enable*",
+#        "kms:List*",
+#        "kms:Put*",
+#        "kms:Update*",
+#        "kms:Revoke*",
+#        "kms:Disable*",
+#        "kms:Get*",
+#        "kms:Delete*",
+#        "kms:ScheduleKeyDeletion",
+#        "kms:CancelKeyDeletion"
+#      ],
+#      "Resource": "*"
+#    }
+#  ]
+#}
+#EOF
+#}
 
 resource "aws_iam_role_policy" "lambda_network_access" {
   name = "${var.project_name}-lambda-network-access-policy"
@@ -80,7 +167,6 @@ resource "aws_iam_role_policy" "lambda_network_access" {
 }
 EOF
 }
-
 
 resource "aws_iam_role_policy" "lambda_s3_access" {
   name = "${var.project_name}-lambda-s3-access-policy"
